64aaee0499b4a5ae9e988ad54cfec9bedc3c15d3
Random-Scientist <Random-Scientist@users.noreply.github.com>
2025-11-17 16:22:49 -0800

    features enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
-
+pub mod stuff;
+pub mod util;
---
+++ src/stuff.rs
@@ -0,0 +1,52 @@
+use crate::util::bitflag_bits;
+
+bitflag_bits! {
+    /// derived from https://emanuelecozzi.net/docs/airplay2/features
+    #[derive(Debug, Clone, Copy)]
+    pub struct AirplayFeatures: u64
+    bits: {
+        SUPPORTS_VIDEO_V1: 0,
+        SUPPORTS_PHOTO: 1,
+        SUPPORTS_SLIDESHOW: 5,
+        FLAG6_SUPPORTS_FROM_CLOUD: 6,
+        SUPPORTS_SCREEN: 7,
+        SUPPORTS_AUDIO: 9,
+        AUDIO_REDUNDANT: 11,
+        AUTHENTICATION_4: 14,
+        METADATA_FEATURES_0: 15,
+        METADATA_FEATURES_1: 16,
+        METADATA_FEATURES_2: 17,
+        AUDIO_FORMATS_0: 18,
+        AUDIO_FORMATS_1: 19,
+        AUDIO_FORMATS_2: 20,
+        AUDIO_FORMATS_3: 21,
+        AUTHENTICATION_1: 23,
+        AUTHENTICATION_8: 26,
+        SUPPORTS_LEGACY_PAIRING: 27,
+        HAS_UNIFIED_ADVERTISER_INFO: 30,
+        IS_CARPLAY: 32, // supportsvolume = !IS_CARPLAY
+        SUPPORTS_VIDEO_QUEUE: 33,
+        SUPPORTS_FROM_CLOUD: 34, // && FLAG6_SUPPORTS_FROM_CLOUD
+        SUPPORTS_TLS_PSK: 35,
+        SUPPORTS_UNIFIED_MEDIA_CONTROL: 38,
+        SUPPORTS_BUFFERED_AUDIO: 40,
+        SUPPORTS_PTP: 41,
+        SUPPORTS_SCREEN_MULTI_CODEC: 42,
+        SUPPORTS_SYSTEM_PAIRING: 43,
+        // tf is this
+        IS_AP_VALERIA_SCREEN_SENDER: 44,
+        SUPPORTS_HK_PAIRING_AND_ACCESS_CONTROL: 46,
+        SUPPORTS_CORE_UTILS_PAIRING_AND_ENCRYPTION: 48, // 38 || 46 || 43 || 48
+        SUPPORTS_VIDEO_V2: 49,
+        METADATA_FEATURES_3: 50,
+        SUPPORTS_UNIFIED_PAIR_SETUP_AND_MFI: 51,
+        SUPPORTS_SET_PEERS_EXTENDED_MESSAGE: 52,
+        SUPPORTS_AP_SYNC: 54,
+        SUPPORTS_WOL_0: 55, // 55 || 56
+        SUPPORTS_WOL_1: 56, // 55 || 56
+        SUPPORTS_HANGDOG_REMOTE_CONTROL: 58,
+        SUPPORTS_AUDIO_STREAM_CONNECTION_SETUP: 59,
+        SUPPORTS_AUDIO_MEDIA_DATA_CONTROL: 60,
+        SUPPORTS_RFC2198_REDUNDANCY: 61,
+    }
+}
